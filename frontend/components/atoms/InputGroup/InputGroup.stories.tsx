78e04c540336eae0eed14c2933a68b8f4e286d04
Pyrhagore <Pythagore@gmail.com>
2025-08-07 00:40:52 +0100

    Ajout du composant InputGroup

---
+++ frontend/components/atoms/InputGroup/InputGroup.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+import InputGroup from './InputGroup';
+import { InputGroupProps } from './types';
+
+export default {
+  title: 'Atoms/InputGroup',
+  component: InputGroup,
+} as Meta;
+
+const Template: Story<InputGroupProps> = (args) => <InputGroup {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  placeholder: 'Rechercher...',
+  buttonText: 'Go',
+};
+
+export const WithCustomText = Template.bind({});
+WithCustomText.args = {
+  placeholder: 'Entrez votre email',
+  buttonText: 'Souscrire',
+};
