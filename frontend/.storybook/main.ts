ec62847bc4a0e9cbd96885789f7e0cb400071bbb
Pythagore Emmanuel ADJALLA <146584727+Pyrhagore@users.noreply.github.com>
2025-08-06 20:42:15 +0100

    Update main.ts

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -12,6 +12,7 @@ const config: StorybookConfig = {
     "@storybook/addon-a11y",
     "@storybook/addon-vitest"
   ],
+    staticDirs: ['../public'],
   "framework": {
     "name": "@storybook/nextjs-vite",
     "options": {}
