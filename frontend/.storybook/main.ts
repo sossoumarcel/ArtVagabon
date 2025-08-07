85e7a4d2f732c53137fb6bde2b79687a9a5aae28
Pythagore <pythagoreadjalla@gmail.com>
2025-08-07 10:26:08 +0100

    modifs

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -1,23 +1,18 @@
-import type { StorybookConfig } from "@storybook/nextjs-vite";
+import type { StorybookConfig } from "@storybook/nextjs-vite"
 
 const config: StorybookConfig = {
-  stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-    "../components/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
+  // LIGNE CORRECTE
+  stories: ["../components/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@chromatic-com/storybook",
     "@storybook/addon-docs",
     "@storybook/addon-a11y",
     "@storybook/addon-vitest",
   ],
-  "framework": {
-    "name": "@storybook/nextjs-vite",
-    "options": {}
+  framework: {
+    name: "@storybook/nextjs-vite",
+    options: {},
   },
-  staticDirs: ["../public", "../stories/assets"],
-};
-
-export default config;
-
+  staticDirs: ["..\\public"],
+}
+export default config
