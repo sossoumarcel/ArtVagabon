ac7d9f00f5577ea137d66f3cb5e81147a448a169
Pyrhagore <Pythagore@gmail.com>
2025-08-11 23:29:28 +0100

    footer mobile

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -8,20 +8,13 @@ const config: StorybookConfig = {
     "@storybook/addon-docs",
     "@storybook/addon-a11y",
     "@storybook/addon-vitest",
-<<<<<<< HEAD
-=======
     
 
->>>>>>> 24258d5ac3b788d20d9a083f204565eb022bc840
   ],
   framework: {
     name: "@storybook/nextjs-vite",
     options: {},
   },
-<<<<<<< HEAD
-  staticDirs: ["..\\public"],
-=======
   staticDirs: ["../public"],
->>>>>>> 24258d5ac3b788d20d9a083f204565eb022bc840
 }
 export default config
