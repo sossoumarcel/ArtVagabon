11d3536fb30c255ca3a4afcf92eedd2c6dbd0ed1
Pythagore <pythagoreadjalla@gmail.com>
2025-08-12 08:02:16 +0100

    Modifs patcs

--- frontend/components/atoms/hero/HeroBanner.jsx
+++ frontend/components/atoms/hero/HeroBanner.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 const HeroBanner = ({ title, subtitle, backgroundImage, buttonText, buttonLink }) => {
   const hasImage = Boolean(backgroundImage);
 
